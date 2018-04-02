14fd3b646f2e5ef5269d2b554ae9cbb7d1347201
Anurag Tyagi <anurag.tyagi.cse16@itbhu.ac.in>
2018-04-02 19:21:56 +0530

    completed

--- checkimage.py
+++ checkimage.py
@@ -1,30 +1,28 @@
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
-f = open('new_training.txt','r')
-
+f = open('new_training_frontal.txt','r')
+pred=np.load('output.npy')
+a=np.ndarray.tolist(pred)
 i=0
 for l in f:
-	if i > 10:
-		break
-	i+=1
-	l = l.strip()
-	l = l.split()
-	if len(l) < 10:
-		break
-	add = l[0]
-	x = list(map(float,l[1:6]))
-	y = list(map(float,l[6:11]))
-	img = Image.open(add).convert('RGBA')
-	zipped = list(zip(x,y))
-	
-	new = Image.new('RGBA', img.size, (255,255,255,0))
-	imgd = ImageDraw.Draw(new)
+	if i >= 2500 and i<4000:
+        l = l.strip()
+        l = l.split()
+        add = l[0]
+        x = list(map(float,a[i-2500][0:5]))
+        y = list(map(float,a[i-2500][5:10]))
+        img = Image.open(add).convert('RGBA')
+        zipped = list(zip(x,y))
 
-	imgd.point(zipped,fill='red')
+        new = Image.new('RGBA', img.size, (255,255,255,0))
+        imgd = ImageDraw.Draw(new)
 
-	out = Image.alpha_composite(img, new)
-	out = out.resize((400,400))
-	out.show()
+        imgd.point(zipped,fill='red')
 
-
+        out = Image.alpha_composite(img, new)
+        out = out.resize((400,400))
+        out.show()
+    i+=1
+f.close()
 	
